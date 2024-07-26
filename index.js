c5bd3559214df3955cd9365962ad9ca85f3794cd
Palak <rpalak317@gmail.com>
2024-07-26 18:08:15 +0530

    Building Authentication from scratch

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const { connectToMongoDB } = require('./connect');
-const path = require('path');
+const URL = require("./models/url");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const {restrictToLoggedinUserOnly, checkAuth} = require("./middlewares/auth")
 const urlRoute = require('./routes/url');
 const staticRoute = require('./routes/staticRouter');
+const userRouter = require("./routes/user")
+
 const app = express();
 const PORT = 8001;
 
@@ -16,9 +21,11 @@ app.set('views', path.resolve('./views'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-app.use('/url', urlRoute);
-app.use('/', staticRoute);
+app.use('/url', restrictToLoggedinUserOnly,urlRoute);
+app.use('/user', userRouter);
+app.use('/',checkAuth, staticRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`);
